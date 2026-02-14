ea48e9aeb947e673e163da7d3702a216c278090a
beenehdev <beenehdev@gmail.com>
2026-02-14 00:25:35 -0800

    Add initialized variables

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,14 @@
 module Hangman
   # Responsible for orchestrating gameflow of hangman
   class Game
-    raise NotImplementedError
+    def initialize(interface, dictionary, save_manager)
+      @interface = interface
+      @dictionary = dictionary
+      @save_manager = save_manager
+    end
+
+    def start
+      @interface.welcome
+    end
   end
 end
