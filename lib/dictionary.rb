32d63b9b96ad44f923561215157cedd5b7bd384d
beenehdev <beenehdev@gmail.com>
2026-02-13 19:48:12 -0800

    Add data library for dictionary.rb

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -3,6 +3,16 @@
 module Hangman
   # Responsible for providing word list from file
   class Dictionary
-    raise NotImplementedError
+    def initialize
+      File.open('../data/google-10000-english-no-swears.txt', 'r') do |file|
+        @list = file.readlines
+                    .map(&:chomp)
+                    .select { |word| word.length.between?(5, 12) }
+      end
+    end
+
+    def random_word
+      @list.sample
+    end
   end
 end
