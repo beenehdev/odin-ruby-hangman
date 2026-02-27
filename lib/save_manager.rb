075978282ffb90ff604fbbc15a6147c7f920aef5
beenehdev <beenehdev@gmail.com>
2026-02-27 14:23:10 -0800

    Add pseudocode save_fnc

--- lib/save_manager.rb
+++ lib/save_manager.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
+require 'json'
+
 module Hangman
   # Responsible for saving and loading game data
   class SaveManager
-    raise NotImplementedError
+    attr_reader :save_exists
+
+    def initialize(stuff, things)
+      @save_exists = true if document(has_savedata)
+      @data_snapshot = [stuff, things]
+    end
+
+    def save
+      File.write('../saves/save.json') do |f|
+        f.write(@data_snapshot.to_json)
+      end
+    end
+
+    def load
+      file = File.read('../saves/save.json', &:read)
+      data_hash = JSON.parse(file)
+    end
   end
 end
